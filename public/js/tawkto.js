d7777126353be6bfc5a3e430f4988867e2c271cd
Robin <dev@scanandstock.fr>
2026-06-17 15:39:46 +0200

    feat: replace Font Awesome icons with inline SVG icons for improved performance and accessibility

--- public/js/tawkto.js
+++ public/js/tawkto.js
@@ -1,13 +1,15 @@
 (function initTawkTo() {
-  var Tawk_API = Tawk_API || {},
-    Tawk_LoadStart = new Date();
-  (function () {
-    var s1 = document.createElement("script"),
-      s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = "https://embed.tawk.to/68540aaaa39e6f190afde496/1iu44jb22";
-    s1.charset = "UTF-8";
-    s1.setAttribute("crossorigin", "*");
-    s0.parentNode.insertBefore(s1, s0);
-  })();
+  // Exposés sur window pour que l'embed Tawk.to ET toute config ultérieure
+  // (Tawk_API.visitor, Tawk_API.onLoad…) partagent bien le même objet global.
+  window.Tawk_API = window.Tawk_API || {};
+  window.Tawk_LoadStart = new Date();
+
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/68540aaaa39e6f190afde496/1iu44jb22";
+  s1.charset = "UTF-8";
+  // Valeur CORS valide ("*" n'est pas autorisé pour l'attribut crossorigin).
+  s1.setAttribute("crossorigin", "anonymous");
+  s0.parentNode.insertBefore(s1, s0);
 })();
